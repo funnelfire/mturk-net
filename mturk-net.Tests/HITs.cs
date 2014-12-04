bc1c5485f111aa2fc6e9d147747fa9bef9a4b9df
David Pfeffer <bytenik@bytenik.com>
2014-12-04 09:26:19 -0500

    create a HIT test passes!

--- mturk-net.Tests/HITs.cs
+++ mturk-net.Tests/HITs.cs
@@ -16,13 +16,13 @@ namespace MTurk.Tests
             var client = new TurkClient(Credentials.AccessKey, Credentials.SecretKey, true);
             var resp = await client.CreateHIT("Test HIT", "A test HIT", new Price {Amount = 0.07M, CurrencyCode = "USD"},
                 TimeSpan.FromDays(7), null, TimeSpan.FromDays(7), null,
-                new ExternalQuestion {ExternalURL = "http://www.google.com/", FrameHeight = "100px"},
+                new ExternalQuestion {ExternalURL = "https://www.google.com/", FrameHeight = "100"},
                 TimeSpan.FromDays(7));
 
             if (resp.OperationRequest != null && resp.OperationRequest.Errors != null && resp.OperationRequest.Errors.Length != 0)
                 Assert.False(true, string.Format("Global operation error: [{0}] {1}", resp.OperationRequest.Errors[0].Code, resp.OperationRequest.Errors[0].Message));
 
-            if (resp.HIT[0].Request.Errors.Length != 0)
+            if (resp.HIT[0].Request.Errors != null && resp.HIT[0].Request.Errors.Length != 0)
                 Assert.False(true, string.Format("HIT operation error: [{0}] {1}", resp.HIT[0].Request.Errors[0].Code, resp.HIT[0].Request.Errors[0].Message));
         }
     }
